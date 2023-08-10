1184f431c52304a24864d726e88c21f5c1dd322b
MATHIEU VAN DAMME <vandammemathieu@hotmail.fr>
2023-08-10 11:44:00 +0200

    F03 : LOGIN > Added background img

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,8 +1,19 @@
 import LoginForm from "./LoginForm";
+import styled from "styled-components";
 
 const LoginPage = () => {
+  return (
+    <Wrapper>
+      <LoginForm />
+    </Wrapper>
+  );
+};
 
-    return <LoginForm />;
-}
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: url("assets/F03 burger-background.webp") no-repeat center;
+  background-size: 100%;
+`;
 
-export default LoginPage
+export default LoginPage;
