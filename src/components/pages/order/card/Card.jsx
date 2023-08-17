ced3d5dd05d90fb471b10df26cc20c89ba01940e
MATHIEU VAN DAMME <vandammemathieu@hotmail.fr>
2023-08-17 21:41:05 +0200

    F05: ORDER > ProductCard > Refacto : create <BottomCard />

--- src/components/pages/order/card/Card.jsx
+++ src/components/pages/order/card/Card.jsx
@@ -1,21 +1,13 @@
 import { styled } from "styled-components";
 import { theme } from "../../../../assets/theme";
-import Button from "../../../reusable-ui/Button";
-import { formatPrice } from "../../../../utils/maths";
 import TopCard from "./TopCard";
+import BottomCard from "./BottomCard";
 
 export const Card = ({ imageSource, title, price }) => {
   return (
     <CardStyled>
       <TopCard image={imageSource} title={title} />
-      
-      <div className='bottomCard'>
-        <h2>{title}</h2>
-        <div className='priceAndButton'>
-          <p>{formatPrice(price)}</p>
-          <Button label='Ajouter' className='CTAbutton' />
-        </div>
-      </div>
+      <BottomCard price={price} title={title} />
     </CardStyled>
   );
 };
@@ -33,36 +25,4 @@ const CardStyled = styled.div`
   padding: 50px 20px 10px 20px;
   border-radius: ${theme.borderRadius.extraRound};
   background: ${theme.colors.background_white};
-
-  div.bottomCard {
-    h2 {
-      color: #17161a;
-      font-family: Amatic SC;
-      font-size: 36px;
-      font-style: normal;
-      font-weight: 700;
-      width: 180px;
-      display: block;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      overflow: hidden;
-    }
-
-    div.priceAndButton {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-top: 10px;
-
-      p {
-        color: ${theme.colors.primary};
-      }
-
-      .CTAbutton {
-        width: 95px;
-        height: 38px;
-        padding: 12px 26.3px 12px 26.5px;
-      }
-    }
-  }
 `;
--- src/components/pages/order/card/TopCard.jsx
+++ src/components/pages/order/card/TopCard.jsx
@@ -3,20 +3,22 @@ import { styled } from "styled-components";
 const TopCard = ({ image, title }) => {
   return (
     <TopCardStyled>
-      <img
-        src={image}
-        alt={`image du produit ${title}`}
-        width='200px'
-        height='145px'
-      />
+      <img src={image} alt={`image du produit ${title}`} />
     </TopCardStyled>
   );
 };
 
 const TopCardStyled = styled.div`
   margin: 0 auto;
-  width: 200px;
+  width: 190px;
   height: 145px;
+
+  display: flex;
+  justify-content: center;
+
+  img {
+    height: 100%;
+  }
 `;
 
 export default TopCard;
